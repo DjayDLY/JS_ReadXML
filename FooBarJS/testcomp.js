f8d5240dc9117de01e98a3046f1c70eac318ec93
Jerome.Boucher.Veilleux <jerome.boucher-veilleux@usherbrooke.ca>
2020-06-11 02:33:38 -0400

    poc

--- FooBarJS/testcomp.js
+++ FooBarJS/testcomp.js
@@ -1,84 +1,94 @@
-/**
- * Javascript vanilla dynanmic html element maker
- */
 
-// HTML element
+/** ===========================================================================
+ * @author:JJ
+ *  Just a spec for html comp
+ * ============================================================================*/
 let el =[
-	{
-		type:'label',
-		id:'idLabel',
-		value:'title',
-	},
-	{
-		type:'input',
-		id:'idInput',
-		value:'value ...',
-		events:{
-			blur:foo, 				// normal-init
-			change: ()=>{			// lazy-init
-				console.log(getCompValue("idInput"));
-			},
+		{
+			type:'div',
+			components:
+			[
+			    {
+					type:'p',
+					id:'idNucl',
+					value:'nuclear launch',
+					events:{
+						mouseover: ()=> {
+							console.log("onmouseflyover");
+						}
+					}
+			    },
+			    {
+					type:'div',
+					components:[
+						{   
+							type:'label',
+							id: 'idCompPoc',
+							value:'1-2 attention please',
+							events:	{
+								mouseover:()=>{
+									let x = document.getElementById("errorLabel");
+									x.innerText = "YUPPPPP";
+									x.style.color = "RED";
+									x.style.visibility = "visible";
+								},
+								mouseout:()=>{
+									let x = document.getElementById("errorLabel");
+									x.style.visibility = "hidden";
+								}
+							}
+				    	}
+					]
+			    }
+			]
+		},
+		{
+			type:'label',
+			value:'title',
+		},
+		{
+			type:'input',
+			id: 'idInput1',
+			value:'value ...',
+			events:{
+				change: ()=>{
+		       		 let v = document.getElementById("idCompPoc");
+		       		 v.innerText = document.getElementById("idInput1").value;
+				},
+			}
+		},
+		{
+			type:'label',
+			id:'idInput1',
+			value:'title2',
+		},
+		{
+			type:'input',
+			id:'idInput2',
+			value:'value ...',
+			events:{
+				change:()=>{
+					console.log("can bind anonymous");
+				},
+			}
+		},
+		{
+			type:'label',
+			id:'errorLabel',
+			value:'',
+		},
+		{
+			type:'button',
+			id:'idButton',
+			value:'ok',
+			events:{
+				click:()=>{
+					alert("clicked");	
+				}
+			}
 		}
-	},
-	{
-		type:'label',
-		id:'idLabel2',
-		value:'title2',
-	},
-	{
-		type:'input',
-		id:'idInput2',
-		value:'value ...',
-		events:{
-			blur:foo,
-			blur:foo2,
-			change:()=>{
-				console.log("can bind anonymous");
-				foo3(getCompValue("idInput2")); // bind function in function that even take param
-			},
-		}
-	},
-	{
-		type:'label',
-		id:'errorLabel',
-		value:'',
-	},
-	{
-		type:'button',
-		id:'idButton',
-		value:'ok',
-		events:{
-			click:bar,
-		}
-	}
-]
+	]
 
-// Function for html tag
-function foo(){
-	console.log(baz());
-}
-function foo2(){
-	console.log("can bind more than 1 function");
-}
-function foo3(x){
-	console.log("with param " + x);
-}
-
-function getCompValue(id){
-	return document.getElementById(id).value;
-}
-
-function bar(){
-	let v = document.getElementById("errorLabel");
-	if(v.innerText === '')
-		v.innerText = baz();
-	else
-		v.innerText = '';
-}
-function baz(){
-	return document.getElementById("idInput").value +
-		document.getElementById("idInput2").value;
-}
 
 /** ===========================================================================
  * Just create element and bind events:
@@ -88,20 +98,30 @@ let build = (components) => {
 	for (let elem in components) {
 
 		// Create Element could easily
-		// Change at need
 		let e = document.createElement(components[elem].type);
-		e.id = components[elem].id;
-		e.innerText = e.placeholder = components[elem].value;
+        
+		// if id is defined set it
+		if (components[elem].id)
+		    e.id = components[elem].id;
+
+		// if value is defined set it        
+		if (components[elem].value)
+		    e.innerText = e.placeholder = components[elem].value;
 
 		// Map event with associate function then bind them
-		if(components[elem].events){
+		if(components[elem].events)
 			Object.keys(components[elem].events).map(function(key){
 				return [String(key), components[elem].events[key]];
 			}).forEach(ev=>{
 				e.addEventListener(ev[0],ev[1]);
 			});
-		}
-
+		
+		// Recursive call to append child
+		if (components[elem].components)
+		    build(components[elem].components).forEach(child => {
+		        e.appendChild(child);
+		    });
+		
 		// Add event to return array
 		child.push(e);
 	}
@@ -110,9 +130,9 @@ let build = (components) => {
 
 
 /** ===========================================================================
- * Just adding them to DOM
+ *  Just adding the array to the body
  * ============================================================================*/
 build(el).forEach(comp => {
 	document.getElementsByTagName('body')[0].appendChild(comp);
-	document.getElementsByTagName('body')[0].appendChild(document.createElement('br'));
+	document.getElementsByTagName('body')[0].appendChild(document.createElement("br"));
 });
